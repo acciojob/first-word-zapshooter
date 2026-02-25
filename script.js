6bdb7f198668f70704f3c22ce3698e2138f168aa
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-02-25 23:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
-  // your code here
+  const trimmed = s.trimStart();
+  const idx = trimmed.indexOf(' ');
+  return idx === -1 ? trimmed : trimmed.substring(0, idx);
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
